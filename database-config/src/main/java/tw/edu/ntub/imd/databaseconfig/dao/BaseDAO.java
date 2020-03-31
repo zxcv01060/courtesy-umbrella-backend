68f450af15d4e3db1911f5b69661f7a54325bee8
EnWei <10646007@ntub.edu.tw>
2020-03-31 09:43:45 +0800

    調整 - 忽略insert回傳參數未使用的警告

--- database-config/src/main/java/tw/edu/ntub/imd/databaseconfig/dao/BaseDAO.java
+++ database-config/src/main/java/tw/edu/ntub/imd/databaseconfig/dao/BaseDAO.java
@@ -3,6 +3,7 @@ package tw.edu.ntub.imd.databaseconfig.dao;
 import javax.annotation.Nonnull;
 
 public interface BaseDAO<ID, E> extends BaseViewDAO<ID, E> {
+    @SuppressWarnings("UnusedReturnValue")
     @Nonnull
     E insert(@Nonnull E e);
 
