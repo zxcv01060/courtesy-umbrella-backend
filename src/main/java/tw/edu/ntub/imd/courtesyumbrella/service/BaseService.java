4f4e4add453770cc02dd3ebbc8886a9ccbc4eee1
EnWei <10646007@ntub.edu.tw>
2020-03-31 11:30:00 +0800

    調整 - 調整BaseService參數名稱

--- src/main/java/tw/edu/ntub/imd/courtesyumbrella/service/BaseService.java
+++ src/main/java/tw/edu/ntub/imd/courtesyumbrella/service/BaseService.java
@@ -3,9 +3,9 @@ package tw.edu.ntub.imd.courtesyumbrella.service;
 import tw.edu.ntub.imd.courtesyumbrella.bean.BaseBean;
 
 public interface BaseService<B extends BaseBean, ID> extends BaseViewService<B, ID> {
-    void create(B bean);
+    void create(B b);
 
-    void update(ID id, B bean);
+    void update(ID id, B b);
 
     void delete(ID id);
 }
