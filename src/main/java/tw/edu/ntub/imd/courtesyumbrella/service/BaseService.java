f24c8760f7b30903c9a02b2aa1771c3785b0b1b2
EnWei <10646007@ntub.edu.tw>
2020-03-31 09:14:11 +0800

    調整 - 調整BaseService#createAndReturnBean的名稱為create

--- src/main/java/tw/edu/ntub/imd/courtesyumbrella/service/BaseService.java
+++ src/main/java/tw/edu/ntub/imd/courtesyumbrella/service/BaseService.java
@@ -3,7 +3,7 @@ package tw.edu.ntub.imd.courtesyumbrella.service;
 import tw.edu.ntub.imd.courtesyumbrella.bean.BaseBean;
 
 public interface BaseService<B extends BaseBean, ID> extends BaseViewService<B, ID> {
-    B createAndReturnBean(B bean);
+    void create(B bean);
 
     void update(ID id, B bean);
 
