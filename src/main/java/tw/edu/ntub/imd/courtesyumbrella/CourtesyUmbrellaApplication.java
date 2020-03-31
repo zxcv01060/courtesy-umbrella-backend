09ded885494fde138d9e0433a84f57ec522e312f
EnWei <10646007@ntub.edu.tw>
2020-03-31 08:32:59 +0800

    修正 - 修正Spring未將Controller掃描到的問題

--- src/main/java/tw/edu/ntub/imd/courtesyumbrella/CourtesyUmbrellaApplication.java
+++ src/main/java/tw/edu/ntub/imd/courtesyumbrella/CourtesyUmbrellaApplication.java
@@ -8,7 +8,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication(scanBasePackages = {"tw.edu.ntub.imd.config", "tw.edu.ntub.imd.databaseconfig"})
+@SpringBootApplication(scanBasePackages = "tw.edu.ntub.imd")
 public class CourtesyUmbrellaApplication {
 
     public static void main(String[] args) {
