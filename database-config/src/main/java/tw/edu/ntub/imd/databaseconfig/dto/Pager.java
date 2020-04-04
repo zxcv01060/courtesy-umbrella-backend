a010015050c0e0e25848cb0d11cc861d95daf206
EnWei <10646007@ntub.edu.tw>
2020-04-04 19:53:42 +0800

    調整 - 刪除Pager中關於Index的計算

--- database-config/src/main/java/tw/edu/ntub/imd/databaseconfig/dto/Pager.java
+++ database-config/src/main/java/tw/edu/ntub/imd/databaseconfig/dto/Pager.java
@@ -1,21 +1,11 @@
 package tw.edu.ntub.imd.databaseconfig.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+@AllArgsConstructor
+@Getter
 public class Pager {
-    @Getter
-    private boolean infinity;
-
-    public int page;
-    public int count;
-
-    public static Pager infinity() {
-        Pager pager = new Pager();
-        pager.infinity = true;
-        return pager;
-    }
-
-    public int getFirstResultIndex() {
-        return (page - 1) * count;
-    }
+    private int page;
+    private int count;
 }
