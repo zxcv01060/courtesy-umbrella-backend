392e3d2dd0dd4f01f804b06eb9ae37686868e0c2
EnWei <10646007@ntub.edu.tw>
2020-04-05 13:22:04 +0800

    新增測試 - 完成UserDAO持久層測試

--- src/test/java/tw/edu/ntub/imd/courtesyumbrella/CourtesyUmbrellaApplicationTest.java
+++ src/test/java/tw/edu/ntub/imd/courtesyumbrella/CourtesyUmbrellaApplicationTest.java
@@ -1,11 +1,7 @@
 package tw.edu.ntub.imd.courtesyumbrella;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.jdbc.Sql;
 
-@Sql("classpath:data.sql")
-@SpringBootTest
 class CourtesyUmbrellaApplicationTest {
     @Test
     void contextLoad() {
